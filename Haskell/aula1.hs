36d73ead2b0e3c955b00eb0759d0a116b178cbac
Leandro Magalhães <leandroafm21@gmail.com>
2018-08-21 22:28:04 -0300

    Moved some exercises to next lesson

--- Haskell/aula1.hs
+++ Haskell/aula1.hs
@@ -1,5 +1,3 @@
--- Os exercícios foram executados através do ambiente virtual repl.it
-
 -- Assuntos da aula: tipos simples, operadores lógicos e aritméticos, listas, funções, append (:), concatenação (++), head e tail
 
 -- 1-) Tamanho de uma lista
@@ -16,17 +14,11 @@ somaPares (x:xs)
   | (mod x 2 == 0) = x + somaPares xs
   | otherwise = somaPares xs
 
--- 4-) Soma dos elementos nas posições pares da lista - versão 1 (usando tam)
-somaIndicesPares1 [] = 0
-somaIndicesPares1 (x:xs)
-  | mod (tam xs) 2 == 0 = x + somaIndicesPares1 xs 
-  | otherwise = somaIndicesPares1 xs
-
--- 4-) Soma dos elementos nas posições pares da lista - versão 2 (sem usar tam)
-somaIndicesPares2 [] i = 0
-somaIndicesPares2 (x:xs) i
-  | mod i 2 == 0 = x + somaIndicesPares2 xs (i+1)
-  | otherwise = somaIndicesPares2 xs (i+1)
+-- 4-) Soma dos elementos nas posições pares da lista
+somaIndicesPares [] = 0
+somaIndicesPares (x:xs)
+  | mod (tam xs) 2 == 0 = x + somaIndicesPares xs 
+  | otherwise = somaIndicesPares xs
 
 -- 5-) Existe item na lista (True/False)
 existeItem [] it = False
@@ -40,33 +32,20 @@ posicaoItem (x:xs) it i
   | x == it = i
   | otherwise = posicaoItem xs it (i+1)
 
--- 7-) Conta quantas vezes item aparece na lista - versão 1 (com acumuladora)
-ocorrenciasItem1 [] it res = res
-ocorrenciasItem1 (x:xs) it res
-  | x == it = ocorrenciasItem1 xs it (res+1)
-  | otherwise = ocorrenciasItem1 xs it res
+-- 7-) Conta quantas vezes item aparece na lista
+ocorrenciasItem [] it = 0
+ocorrenciasItem (x:xs) it
+  | x == it = 1 + ocorrenciasItem xs it
+  | otherwise = ocorrenciasItem xs it
 
--- 7-) Conta quantas vezes item aparece na lista - versão 2 (sem acumuladora)
-ocorrenciasItem2 [] it = 0
-ocorrenciasItem2 (x:xs) it
-  | x == it = 1 + ocorrenciasItem2 xs it
-  | otherwise = ocorrenciasItem2 xs it
-
--- *8-) Maior elemento de uma lista - versão 1 (com acumuladora)
--- Chamada: maiorElemento (tail l) (head l), l é a lista
-maiorElemento1 [] res = res
-maiorElemento1 (x:xs) res
-  | x > res = maiorElemento1 xs x
-  | otherwise = maiorElemento1 xs res
-
--- *8-) Maior elemento de uma lista - versão 2 (sem acumuladora)
-maiorElemento2 [] = []
-maiorElemento2 [x] = x
-maiorElemento2 (x:xs)
-  | maiorElemento2 xs > x = maiorElemento2 xs
+-- 8-) Maior elemento de uma lista
+maiorElemento [] = []
+maiorElemento [x] = x
+maiorElemento (x:xs)
+  | maiorElemento xs > x = maiorElemento xs
   | otherwise = x
 
--- *9-) Reverte uma lista
+-- 9-) Reverte uma lista
 reverte [] = []
 reverte (x:xs) = (reverte xs) ++ [x]
 
