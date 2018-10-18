d9073b621601bc109b3ff3b4ee7d4eddcfa5fd5c
Leandro Magalhães <leandroafm21@gmail.com>
2018-10-18 20:05:44 -0300

    Adapted exercises to Repl It ambient
    Fixed error on the second solution of the 6th exercise

--- Python/aula17.py
+++ Python/aula17.py
@@ -1,26 +1,17 @@
 # 1-) Criar uma lista com apenas os valores pares de outra lista
-def criaValoresPares():
-    print("Entre com a lista: ", end="")
-    lista1 = [int(x) for x in input().split()]
-
-    lista2 = [x for x in lista1 if x % 2 == 0]
+def criaValoresPares(lista):
+    lista2 = [x for x in lista if x % 2 == 0]
     print(lista2)
 
 # 2-) Criar uma lista com os valores nas posições pares de outra lista
-def criaIndicesPares():
-    print("Entre com a lista: ", end="")
-    lista1 = [int(x) for x in input().split()]
-
-    lista2 = [lista1[i] for i in range(0, len(lista1)) if i % 2 == 1]
+def criaIndicesPares(lista):
+    lista2 = [lista[i] for i in range(0, len(lista)) if i % 2 == 1]
     print(lista2)
 
 # 3-) Criar um dicionário com a contagem de cada elemento de uma lista
-def criarDicionarioContagem():
-    print("Entre com a lista: ", end="")
-    lista1 = [int(x) for x in input().split()]
-
+def criarDicionarioContagem(lista):
     dicionario = {}
-    for x in lista1:
+    for x in lista:
         if x in dicionario.keys():
             dicionario[x] += 1
         else:
@@ -45,12 +36,9 @@ def obtemChaveMaiorValorDicionario():
             break
 
 # 5-) Qual é o elemento mais comum em uma lista
-def elementoMaisComum():
-    print("Entre com a lista: ", end="")
-    lista1 = [int(x) for x in input().split()]
-
+def elementoMaisComum(lista):
     dicionario = {}
-    for x in lista1:
+    for x in lista:
         if x in dicionario.keys():
             dicionario[x] += 1
         else:
@@ -63,44 +51,31 @@ def elementoMaisComum():
             break
 
 # 6-) Uma lista é sublista de outra? [tricky]
-def sublista1():
-    print("Entre com a lista maior: ", end="")
-    lista1 = [x for x in input().split()]
-
-    print("Entre com a lista menor: ", end="")
-    lista2 = [x for x in input().split()]
-
+def sublista1(lista1, lista2):
     lista1string = "".join(lista1)
     lista2string = "".join(lista2)
-    if lista2string in lista1string:
+    if (lista2string in lista1string) or (lista2string in lista1string):
         print("Sim")
     else:
         print("Não")
 
 # 6-) Uma lista é sublista de outra? [normal]
-def sublista2():
-    print("Entre com a lista maior: ", end="")
-    lista1 = [int(x) for x in input().split()]
-
-    print("Entre com a lista menor: ", end="")
-    lista2 = [int(x) for x in input().split()]
+def sublista2(lista1, lista2):
+    if (len(lista2) > len(lista1)):
+      lista1, lista2 = lista2, lista1
 
     for i in range(0, len(lista1)):
         if (lista1[i] == lista2[0]):
+            print("ok")
             if (i + len(lista2) - 1 < len(lista1)):
-                if (lista1[i:(i + len(lista2)+1)] == lista2):
+                print(lista1[i:(i + len(lista2))])
+                if (lista1[i:(i + len(lista2))] == lista2):
                     print("Sim")
                     return
     print("Não")
 
 # 7-) Dado 2 strings, o fim de uma é igual ao começo de outra?
-def fimDeUmaStringIgualAoComecoDeOutra():
-    print("Entre com a primeira string: ", end="")
-    string1 = input()
-
-    print("Entre com a segunda string: ", end="")
-    string2 = input()
-
+def fimDeUmaStringIgualAoComecoDeOutra(string1, string2):
     for i in range(0, len(string1)):
         fim = string1[i:]
         if (len(fim)+1 < len(string2)):
@@ -109,27 +84,3 @@ def fimDeUmaStringIgualAoComecoDeOutra():
                 print("Match de tamanho " + str(len(fim)) + ": " + comeco)
                 return
     print("Não")
-
-while(True):
-    op = input()
-
-    if (op == "e1"):
-        criaValoresPares()
-    elif (op == "e2"):
-        criaIndicesPares()
-    elif (op == "e3"):
-        criarDicionarioContagem();
-    elif (op == "e4"):
-        obtemChaveMaiorValorDicionario();
-    elif (op == "e5"):
-        elementoMaisComum()
-    elif (op == "e6a"):
-        sublista1()
-    elif (op == "e6b"):
-        sublista2()
-    elif (op == "e7"):
-        fimDeUmaStringIgualAoComecoDeOutra();
-    elif (op == "q"):
-        break
-    else:
-        print("Operação invalida! Entre 'q' para sair.")
